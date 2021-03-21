2454ea0699e1d727dda2189000fb470e27139d22
Mark Antao <35659231+markantao@users.noreply.github.com>
2021-03-21 16:08:45 -0400

    Initial Commit

---
+++ Shared/emojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  emojiAppApp.swift
+//  Shared
+//
+//  Created by Mark Antao on 2021-03-21.
+//
+
+import SwiftUI
+
+@main
+struct emojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
